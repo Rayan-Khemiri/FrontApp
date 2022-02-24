68035bd331032972540100b5da5af97ac530abe5
oussama <khhemiri.ousamaa@gmail.com>
2022-02-24 14:26:34 +0100

    app design && components && (sign in/ sign up iheb)

---
+++ constants/Images.js
@@ -0,0 +1,26 @@
+const Onboarding = 'https://images.unsplash.com/photo-1505995433366-e12047f3f144?fit=crop&w=840&q=80';
+const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
+const Products = {
+  'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
+};
+
+const Profile = 'https://images.unsplash.com/photo-1645654411688-69ec220b5c24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=528&q=80';
+const Avatar = 'https://images.unsplash.com/photo-1518725522904-4b3939358342?fit=crop&w=210&q=80';
+
+const Viewed = [
+  'https://i.pinimg.com/originals/70/7c/0b/707c0b9ac99eb476026302a3ac46e22e.jpg',
+  'https://i.pinimg.com/originals/c1/19/06/c11906482bc5115066b8c6dd6fcdd043.jpg',
+  'https://i.pinimg.com/236x/2d/a3/ee/2da3ee81501ae487acb131a9ffd9f184--actors.jpg',
+  'https://i0.wp.com/redanianintelligence.com/wp-content/uploads/2019/12/77185544_210242240002803_7354896220844919469_n.jpg?fit=1080%2C810&ssl=1',
+  'https://media.istockphoto.com/photos/people-working-behind-the-scenes-on-a-film-set-picture-id1028404620?k=20&m=1028404620&s=612x612&w=0&h=-PUVha7IKPpgpGlgp-It9kwk3P5eG_uLU8av5wH7J6M=',
+  'https://images.unsplash.com/photo-1490971774356-7fac993cc438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVoaW5kJTIwdGhlJTIwc2NlbmV8ZW58MHx8MHx8&w=1000&q=80',
+];
+
+export default {
+  Onboarding,
+  Pro,
+  Products,
+  Profile,
+  Viewed,
+  Avatar,
+}
